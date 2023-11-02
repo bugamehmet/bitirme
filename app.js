f06afadc996d573341ba487b55096fd4f5908870
bugamehmet <bugamehmetcan@gmail.com>
2023-11-03 00:00:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = require('./routers/routes')
+const app = express();
+require('dotenv').config();
+app.set('view engine', 'ejs');
+
+app.use(express.static('views'), (error) => {
+	if (error) {throw error && console.log('views klasöründe hata')};
+});
+
+app.use('/assets', express.static('assets'), (error) => {
+	if (error) throw error && console.log('assets klasöründe hata');
+});
+
+app.use('/', router)
+
+app.listen(process.env.PORT, (error) => {
+	if (error) {
+		console.log('server başlatılırken hata oluştu');
+	} else {
+		console.log(`server ${process.env.PORT} portunda başlatıldı`);
+	}
+});
