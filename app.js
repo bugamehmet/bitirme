2c5cd8e980102974f7f34e2760b645b21dc45e79
bugamehmet <bugamehmetcan@gmail.com>
2023-11-03 06:08:54 +0300

    urlencoded-ilk-post

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router = require('./routers/routes');
 const app = express();
 require('dotenv').config();
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('views'), (error) => {
 	if (error) {
--- routers/routes.js
+++ routers/routes.js
@@ -5,4 +5,10 @@ router.get('/', (req, res) => {
 	res.render('anasayfa');
 });
 
+router.post('/', (req, res) => {
+	let x = req.body.gelir
+	console.log(x)
+	res.render('yaslar');
+});
+
 module.exports = router;
