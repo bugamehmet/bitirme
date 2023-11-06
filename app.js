fa44ea56b95bf94b216b75923f04045b88d63b57
bugamehmet <bugamehmetcan@gmail.com>
2023-11-06 04:55:12 +0300

    ejs to html

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = require('./routers/routes');
 const app = express();
 require('dotenv').config();
 app.set('view engine', 'ejs');
@@ -14,7 +13,22 @@ app.use('/assets', express.static('assets'), (error) => {
 	if (error) throw error && console.log('assets klasöründe hata');
 });
 
-app.use('/', router);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/anasayfa.html');
+});
+
+app.post('/', (req, res) => {
+	let yas = req.body.yas
+	console.log(yas)
+	if(yas>6){
+		res.render(`6yas`)
+	}
+	res.render(`${yas}yas`)
+});
+
+app.get('/2yas', (req, res) => {
+  res.sendFile(__dirname + '/views/anasayfa.html');
+});
 
 app.listen(process.env.PORT, (error) => {
 	if (error) {
