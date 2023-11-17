a94e74d9ed94c634d812a6e6395177e330ccf95d
bugamehmet <bugamehmetcan@gmail.com>
2023-11-17 08:09:25 +0300

    2kolaytest1-v1.0

--- public/test1.js
+++ public/test1.js
@@ -1,16 +1,14 @@
 const basla = document.getElementById('basla');
 basla.addEventListener('click', baslatmaFonk);
 
-const resim1 = document.getElementById('resim1');
-resim1.addEventListener('click', resim1fonk);
+const resimler = [];
+for (let i = 1; i<=3; i++){
+	const resim = document.getElementById(`resim${i}`);
+	resim.addEventListener('click', ()=> resimClick(i));
+	resimler.push(resim);
+}
 
-const resim2 = document.getElementById('resim2');
-resim2.addEventListener('click', resim2fonk);
-
-const resim3 = document.getElementById('resim3');
-resim3.addEventListener('click', resim3fonk);
-
-let sesbutton = document.getElementById('ses-button');
+const sesbutton = document.getElementById('ses-button');
 sesbutton.addEventListener('click', tekrardinle);
 
 const nesneler = [
@@ -30,107 +28,40 @@ const nesneler = [
 		xx: '3',
 	},
 ];
-// TODO : Daha fazla nesne eklenerek html daki img leri rastgele atıyarak yapılabilir.
-const x = Math.floor(Math.random() * 3);
 
-function baslatmaFonk() {
+let x;
+
+function baslatmaFonk(){
+	x = Math.floor(Math.random()*3);
 	const resimlerDisplay = document.querySelector('.resimler');
 	ilkses(nesneler[x]);
-	resimlerDisplay.style.display = 'flex';
-	basla.style.display = 'none';
+	resimlerDisplay.style.display = 'flex'
+	basla.style.display = 'none'
 }
-function resim1fonk() {
-	if (resim1.value == nesneler[x].xx) {
-		const veri = { sonuc: 'DOGRU', sorulan: nesneler[x].isim, tiklanan: nesneler[0].isim };
 
-		fetch('/2kolaytest1oyun1', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(veri),
-		})
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error('Hata:', error);
-			});
-		alert('doğru');
+function resimClick(sira){
+	const dogruCevap = nesneler[x].xx;
+	const tiklanan = nesneler[sira-1].isim;
+
+	if(sira == dogruCevap){
+		fetchAndAlert('DOGRU', tiklanan);
+		alert('DOĞRU')
 	} else {
-		const veri = { sonuc: 'YANLIS', sorulan: nesneler[x].isim, tiklanan: nesneler[0].isim };
-
-		fetch('/2kolaytest1oyun1', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(veri),
-		})
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error('Hata:', error);
-			});
-		alert('yanlış');
+		fetchAndAlert('YANLIS', tiklanan);
+		alert('YANLIŞ')
 	}
 }
-function resim2fonk() {
-	if (resim2.value == nesneler[x].xx) {
-		const veri = { sonuc: 'DOGRU', sorulan: nesneler[x].isim, tiklanan: nesneler[1].isim };
-		fetch('/2kolaytest1oyun1', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(veri),
-		})
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error('Hata', error);
-			});
-		alert('dogru');
-	} else {
-		const veri = { sonuc: 'YANLIS', sorulan: nesneler[x].isim, tiklanan: nesneler[1].isim };
 
-		fetch('/2kolaytest1oyun1', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(veri),
-		})
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error('Hata:', error);
-			});
-		alert('yanlış');
-	}
-}
-function resim3fonk() {
-	if (resim3.value == nesneler[x].xx) {
-		const veri = { sonuc: 'DOGRU', sorulan: nesneler[x].isim, tiklanan: nesneler[2].isim };
-		fetch('/2kolaytest1oyun1', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(veri),
-		})
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error('Hata', error);
-			});
-		alert('dogru');
-	} else {
-		const veri = { sonuc: 'YANLIS', sorulan: nesneler[x].isim, tiklanan: nesneler[2].isim };
+function fetchAndAlert(sonuc, tiklanan){
+	const veri = {sonuc, sorulan: nesneler[x].isim, tiklanan}
 
-		fetch('/2kolaytest1oyun1', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(veri),
-		})
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error('Hata:', error);
-			});
-		alert('yanlış');
-	}
+	fetch('/2kolaytest1oyun1', {
+		method : 'POST',
+		headers : {'Content-Type' : 'application/json'},
+		body : JSON.stringify(veri),
+	})
+	.then((response) => response.json())
+	.catch((error)=> console.error('Hata:', error))
 }
 function ilkses(nesne) {
 	let text = `${nesne.isim} hangisidir`;
@@ -140,13 +71,12 @@ function ilkses(nesne) {
 	utterance.voice = window.speechSynthesis.getVoices()[dil];
 	window.speechSynthesis.speak(utterance);
 }
+
 function tekrardinle() {
 	let text = `${nesneler[x].isim} hangisidir`;
 	let utterance = new SpeechSynthesisUtterance();
 	let dil = 'tr-TR';
-
 	utterance.text = text;
 	utterance.voice = window.speechSynthesis.getVoices()[dil];
-
 	window.speechSynthesis.speak(utterance);
 }
